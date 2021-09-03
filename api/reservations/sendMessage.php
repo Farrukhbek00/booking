d09d5d80e4a203737dd6e9796c01ebc46cb034e9
farrukh <farruhzokirov00@gmail.com>
2021-09-04 04:59:44 +0500

    sending email logic on process

--- api/reservations/sendMessage.php
+++ api/reservations/sendMessage.php
@@ -7,40 +7,59 @@
 
     include_once '../../config/database.php';
     include_once '../../class/reservations.php';
+    include_once '../../class/customers.php';
+
+    use PHPMailer\PHPMailer\PHPMailer;
+
+    
 
     $database = new Database();
     $db = $database->getConnection();
 
-    $item = new Reservation($db);
+    $reservation = new Reservation($db);
 
     $data = json_decode(file_get_contents("php://input"));
-    $item->room_id = $data->room_id;
-    $item->start_date = $data->start_date;
-    $item->end_date = $data->end_date;
+    $reservation->room_id = $data->room_id;
+    $reservation->start_date = $data->start_date;
+    $reservation->end_date = $data->end_date;
   
-    $item->findReservation();
+    $reservation->findReservation();
 
-    if($item->no != null){
-        
-        if ($data->customer_id == $item->customer_id) {
+    if ($reservation->no != null) {
+        if ($data->customer_id == $reservation->customer_id) {
             $customer = new Customer($db);
             $customer->id = $data->customer_id;
             $customer->getSingleCustomer();
 
-            if ($customer->email != null) {
-                $emp_arr = array(
-                    "id" =>  $customer->id,
-                    "first_name" => $customer->first_name,
-                    "last_name" => $customer->last_name,
-                    "email" => $customer->email,
-                    "phone" => $customer->phone,
-                    "created" => $customer->created
-                );
-              
-                http_response_code(200);
-                echo json_encode($emp_arr);
-            }
-            print_r("Teng");
+            //Load Composer's autoloader
+            require '../../vendor/autoload.php';  
+            $mail = new PHPMailer(); 
+            $mail->isSMTP();
+            $mail->Host = 'smtp.mailtrap.io';
+            $mail->SMTPAuth = true;
+            $mail->Port = 2525;
+            $mail->Username = 'a60435026f501e'; 
+            $mail->Password = '83052277fb62e6';
+            $mail->SMTPSecure = 'tls';
+
+            $mail->setFrom("dasas@example.com");
+            $mail->addAddress("sdas@example.com");
+            $mail->addReplyTo("1810132fz@example.com", "Reply");
+            $mail->addCC("cc@example.com");
+            $mail->addBCC("bcc@example.com");
+
+            $mail->isHTML(true);
+
+            $mail->Subject = "Subject Text";
+            $mail->Body = "<i>Mail body in HTML</i>";
+            $mail->AltBody = "This is the plain text version of the email content";
+
+            try {
+                $mail->send();
+                // echo "Message has been sent successfully";
+            } catch (Exception $e) {
+                // echo "Mailer Error: " . $mail->ErrorInfo;
+            } 
         } else {
             
         }
